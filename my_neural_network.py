70f4553521670f72cfe91a414ad321ec6a6cf6b2
artainmo <tainmontarthur@icloud.com>
2021-02-11 11:33:00 +0100

    batch

--- my_neural_network.py
+++ my_neural_network.py
@@ -44,29 +44,43 @@ def init_layers(deep_layers, input_nodes, output_nodes):
         layers.append(np.zeros([layer, 1]),)
     return layers
 
+def show_object(name, obj):
+    print(name + ":")
+    for elem in obj:
+        print(elem.shape)
+    print("----------")
+
+
+def init_deep_gradient(copy):
+    ret = []
+    for cpy in copy:
+        ret.append(np.zeros(cpy.shape))
+    return ret
+
 class My_Neural_Network():
     def __init__(self, inputs, expected, deep_layers=1, learning_rate=0.1, n_cycles=1000):
         self.inputs = inputs
         self.expected = expected
         self.predicted = np.zeros(expected.shape)
         self.layers = init_layers(deep_layers, inputs.shape[1], self.expected.shape[1])
-        print("Layers Init:")
-        for layer in self.layers:
-            print(layer.shape)
-        print("----------")
         self.weights = init_weights(self.layers)
-        print("Weight Init:")
-        for layer in self.weights:
-            print(layer.shape)
-        print("----------")
         self.bias = init_bias(self.weights)
-        print("Bias Init: ")
-        for bias in self.bias:
-            print(bias.shape)
-        print("----------")
+        self.__reset_gradients()
         self.alpha = learning_rate
         self.n_cycles = n_cycles
-    
+        self.show_all()
+   
+    def show_all(self):
+        print("---------------------------------------------------------------------------------")
+        show_object("Layer", self.layers)
+        show_object("Weight", self.weights)
+        show_object("Bias", self.bias)
+        show_object("Output gradient weight", self.output_gradient_weight)
+        show_object("Output gradient bias", self.output_gradient_bias)
+        show_object("Deep gradient weight", self.deep_gradient_weight)
+        show_object("Deep gradient bias", self.deep_gradient_bias)
+        print("---------------------------------------------------------------------------------")
+
     #create output based on input and weights and biases
     def forward_propagation(self, inputs):
         self.layers[0] = np.array([inputs])
@@ -91,19 +105,49 @@ class My_Neural_Network():
     #gradient = derivative = slope = rate of change
     #partial derivatives are used to verify how each weight and bias affect the error individually
     def backward_propagation(self, expected):
-        self.weights[-1] = self.weights[-1] - (self.alpha * self.__output_layer_partial_derivatives(expected))
-        self.bias[-1] = self.bias[-1] - (self.alpha * derivative_delta_output_layer(self.predicted, expected))
+        self.output_gradient_weight[0] = self.output_gradient_weight[0] + self.__output_layer_partial_derivatives(expected)
+        self.output_gradient_bias[0] = self.output_gradient_bias[0] + derivative_delta_output_layer(self.predicted, expected)
         for i in range(len(self.weights) - 2, -1, -1): #range starts from last non-output weights until first weights (index 0)
-            self.weights[i] = self.weights[i] - (self.alpha * self.__deep_layer_partial_derivatives(i, expected))
-            self.bias[i] = self.bias[i] - (self.alpha * self.__delta_derivative_deep_layer(i, expected))
-    
-    def fit(self):
+            self.deep_gradient_weight[i] = self.deep_gradient_weight[i] + self.__deep_layer_partial_derivatives(i, expected)
+            self.deep_gradient_bias[i] = self.deep_gradient_bias[i] + self.__delta_derivative_deep_layer(i, expected)
+  
+    def __reset_gradients(self):
+        self.output_gradient_weight = init_deep_gradient([self.weights[-1]])
+        self.output_gradient_bias = init_deep_gradient([self.bias[-1]])
+        self.deep_gradient_weight = init_deep_gradient(self.weights[0:-1])
+        self.deep_gradient_bias = init_deep_gradient(self.bias[0:-1])
+
+    def __update_weights(self):
+        self.weights[-1] = self.weights[-1] - (self.alpha * self.output_gradient_weight[0])
+        self.bias[-1] = self.bias[-1] - (self.alpha * self.output_gradient_bias[0])
+        for i in range(len(self.weights) - 2, -1, -1): #range starts from last non-output weights until first weights (index 0)
+            self.weights[i] = self.weights[i] - (self.alpha * self.deep_gradient_weight[i])
+            self.bias[i] = self.bias[i] - (self.alpha * self.deep_gradient_bias[i])
+        self.__reset_gradients()
+ 
+    def __cycle(self, inputs, expected):
+         self.forward_propagation(inputs)
+         self.backward_propagation(expected)
+
+    def __batch_cycle(self):
         for i in range(self.n_cycles):
             for inputs, expected in zip(self.inputs, self.expected):#complete batch cycle
-                self.forward_propagation(inputs)
-                print("Cost: " + str(self.cost(expected)))
-                self.backward_propagation(expected)
+                self.__cycle(inputs, expected)
+            self.show_all()
+            self.__update_weights()
+            self.show_all()
+            print("Cost: " + str(self.cost(expected)))
 
+    def __mini_batch(self):
+        pass
+
+    def __stochastic_cycle(self):
+        shuffle(self.inputs)
+        for i in range(self.n_cycles):
+            self.__cycle(inputs, expected)     
+
+    def fit(self):
+        self.__batch_cycle()   
 
 
 if __name__ == "__main__":
